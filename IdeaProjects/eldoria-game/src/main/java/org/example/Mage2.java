36204e2313593a7cb9b0b5bb8656f2340ceaf97a
irmcm <iremnurceylan@hotmail.com>
2025-04-08 16:07:25 +0300

    Feature - #302: Created Mage2 character with castSpell()`

---
+++ IdeaProjects/eldoria-game/src/main/java/org/example/Mage2.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Mage2 extends Hero implements Questable{
+    public Mage2(String name, int level) {
+        super(name, level);
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("I am " + name + ", a powerful mage of level " + level + "!");
+    }
+
+    @Override
+    public void acceptQuest(String questName) {
+        System.out.println(name + " the mage has accepted the quest: " + questName);
+    }
+
+    public void castSpell() {
+        System.out.println(name + " casts a powerful spell!");
+    }
+}
+
